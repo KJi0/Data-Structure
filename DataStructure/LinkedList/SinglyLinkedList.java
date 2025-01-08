f68489d52fe16d98d30d67af997bb0259b02618a
KJi0 <165020718+KJi0@users.noreply.github.com>
2025-01-08 19:21:02 +0900

    250108 SinglyLinkedList

--- DataStructure/LinkedList/SinglyLinkedList.java
+++ DataStructure/LinkedList/SinglyLinkedList.java
@@ -78,10 +78,10 @@ public class SinglyLinkedList<E> implements List<E> {
 		
 		Node<E> newNode = new Node<E>(value);
 		
-		Node<E> x = search(index - 1);
+		Node<E> preNode = search(index - 1);
 		
-		newNode.next = x.next;
-		x.next = newNode;
+		newNode.next = preNode.next;
+		preNode.next = newNode;
 		size++;
 	}
 	
@@ -95,5 +95,112 @@ public class SinglyLinkedList<E> implements List<E> {
 			tail = null;
 		
 		return item;
+	}
+	
+	@Override
+	public E remove(int index) {
+		
+		if (index == 0)
+			return remove();
+		
+		if (index >= size || index < 0)
+			throw new IndexOutOfBoundsException();
+		
+		Node<E> preNode = search(index - 1);
+		Node<E> removeNode = preNode.next;
+		E item = removeNode.data;
+		
+		preNode.next = removeNode.next;
+		
+		if (preNode.next == null)
+			tail = preNode;
+		
+		size--;
+		
+		return item;
+	}
+	
+	@Override
+	public boolean remove(Object value) {
+		Node<E> preNode;
+		Node<E> x = head;
+		boolean hasValue = false;
+		
+		for (; x != null; x = x.next) {
+			if (value.equals(x.data) {
+				hasValue = true;
+				break;
+			}
+			preNode = x;
+		}
+		
+		if (x == null)
+			return false;
+		
+		if (x.equals(head)) {
+			remove();
+			return true;
+		}
+		
+		else {
+			preNode.next = x.next;
+			
+			if (preNode.next == null)
+				tail = preNode;
+			
+			size--;
+			return true;
+		}
+	}
+	
+	@Override
+	public E get(int index) {
+		return search(index).data;
+	}
+	
+	@Override
+	public void set(int index, E value) {
+		Node<E> replaceNode = search(index);
+		replaceNode.data = value;
+	}
+	
+	@Override
+	public int indexOf(Object value) {
+		int index = 0;
+		
+		for (Node<E> x = head; x != null; x = x.next) {
+			if (value.equals(x.data)) {
+				return index;
+			}
+			index++;
+		}
+		return -1;
+	}
+	
+	@Override
+	public boolean contains(Object item) {
+		return indextOf(item) >= 0;
+	}
+	
+	@Override
+	public int size() {
+		return size;
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	@Override
+	public void clear() {
+		for (Node<E> x = head; x != null) {
+			Node<E> nextNode = x.next;
+			x.data = null;
+			x.next = null;
+			x = nextNode;
+		}
+		head = tail = null;
+		size = 0;
 	}
 }
